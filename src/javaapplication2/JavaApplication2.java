4e1878896f7f5c7601837dc34591f5721ab58f3b
kaur9928 <Kaur9928@sheridancollege.ca>
2020-06-05 20:05:54 -0400

    first commit

---
+++ src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,21 @@
+/*
+ * Your Jaskaran Kaur
+ * Student ID: 991 609 049
+ * SYST10199 - Web Programming
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author Owner
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("java");
+    }
+    
+}
